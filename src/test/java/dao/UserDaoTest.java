e29dc55d954dc54ea84bd80c12736555b2edd574
chordpli <chordplaylist@gmail.com>
2022-10-25 13:51:12 +0900

    221025 getCount()테스트

--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -3,6 +3,7 @@ package dao;
 import domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,8 +44,27 @@ class UserDaoTest {
 
         userDao.add(user01);
         assertEquals(1, userDao.getCount());
-
     }
 
+    @Test
+    @DisplayName("getCount() Test")
+    public void count() throws SQLException, ClassNotFoundException {
+        User user01 = new User("junho", "김준호", "1234");
+        User user02 = new User("pli", "플리", "1234");
+        User user03 = new User("chord", "코드", "1234");
 
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user01);
+        assertEquals(1, userDao.getCount());
+
+        userDao.add(user02);
+        assertEquals(2, userDao.getCount());
+
+        userDao.add(user03);
+        assertEquals(3, userDao.getCount());
+
+        
+    }
 }
