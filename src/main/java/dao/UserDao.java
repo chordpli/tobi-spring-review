782cbe8c7f08cf54219efb38e990e0a02fbcc0da
chordpli <chordplaylist@gmail.com>
2022-10-25 15:05:18 +0900

    221025 익명 내부 클래스

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -24,19 +24,18 @@ public class UserDao {
     }
 
     public void add(final User user) throws SQLException, ClassNotFoundException {
-        class AddStatement implements StatementStrategy{
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
+        jdbcContextWithStatementStrategy(
+                new StatementStrategy() {
+                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
+                        ps.setString(1, user.getId());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getPassword());
 
-                return ps;
-            }
-        }
-        StatementStrategy st = new AddStatement();
-        jdbcContextWithStatementStrategy(st);
+                        return ps;
+                    }
+                }
+        );
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
@@ -99,8 +98,14 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        jdbcContextWithStatementStrategy(
+                new StatementStrategy() {
+                    @Override
+                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                        return c.prepareStatement("delete from users");
+                    }
+                }
+        );
     }
 
     public int getCount() throws SQLException {
