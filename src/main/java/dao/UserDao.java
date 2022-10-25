93bee21947bb78aba17790c48a8c7bf3ef72ed01
chordpli <chordplaylist@gmail.com>
2022-10-25 14:42:06 +0900

    221025 DI 적용을 위한 클라이언트/컨텍스트 분리

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -70,16 +70,16 @@ public class UserDao {
         return user;
     }
 
-    public void deleteAll() throws SQLException {
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;
+
         try {
             c = dataSource.getConnection();
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
+            ps = stmt.makePreparedStatement(c);
             ps.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw e;
         } finally {
             if (ps != null) {
                 try {
@@ -97,6 +97,11 @@ public class UserDao {
                 }
             }
         }
+    }
+
+    public void deleteAll() throws SQLException {
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException {
--- src/main/java/dao/UserDaoDeleteAll.java
+++ src/main/java/dao/UserDaoDeleteAll.java
@@ -5,9 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class UserDaoDeleteAll extends UserDao{
-    @Override
+    /*@Override
     protected PreparedStatement makeStatement(Connection c) throws SQLException {
         PreparedStatement ps = c.prepareStatement("delete from users");
         return ps;
-    }
+    }*/
 }
